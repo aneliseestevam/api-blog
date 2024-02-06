570709a909f3c368b2e8ba5217eb674e8c26e5fd
Anelise Estevam <aestevam.dev@gmail.com>
2024-02-06 06:20:17 -0300

    Production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,8 +5,9 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite3', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
 })
 
 const _env = envSchema.safeParse(process.env)
